92a80c15b433836f679d0196936f4bb604c7cc2f
Emanuel Oliveira <eoliveiracorreia417@gmail.com>
2023-08-23 17:36:58 +0100

    primeiro commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,55 @@
+
 const joao = document.querySelector('.joao')
 const pipe = document.querySelector('.pipe')
+const perdeu = document.querySelector('.lose')
+const oleSound = document.querySelector('.ole')
+const oofSound = document.querySelector('.oof')
 const nuvens = document.querySelector('.nuvens')
-const oleSound = document.querySelector('.ole');
-const oofSound = document.querySelector('.oof');
-const perdeuSound = document.querySelector('.perdeu');
+const perdeuSound = document.querySelector('.perdeu')
 let isPerdeuSound = false
 
 const playPerdeuSound = () => {
     if (!isPerdeuSound) {
-      isPerdeuSound = true;
-      perdeuSound.currentTime = 0;
-      perdeuSound.play();
+      isPerdeuSound = true
+      perdeuSound.currentTime = 0
+      perdeuSound.play()
     }
   };
 
-const jump = () => {
-    joao.classList.add('jump')
+  const jump = () => {
+    if (!isPerdeuSound) { // Check if the game has not ended yet
+        joao.classList.add('jump');
 
         setTimeout(() => {
-        joao.classList.remove('jump')
-    }, 500)
+            joao.classList.remove('jump');
+        }, 500);
 
-    oleSound.currentTime = 0
-    oleSound.play()
-
+        oleSound.currentTime = 0;
+        oleSound.play();
+    }
 }
+
+const restartGame = () => {
+    joao.src = "./imagens/andar.jpg"
+    joao.style.width = "100px"
+    joao.style.animation = "jump 500ms ease-out"
+    joao.style.bottom = `0px`
+    joao.style.left = `0px`
+    // Reinicie as variáveis ou estilos que você precisa
+
+    
+    // Defina a animação do pipe novamente
+    pipe.style.animation = "pipe-animation 1.5s infinite linearntg";
+
+    // Defina a animação das nuvens novamente
+    nuvens.style.animation = "nuvens";
+    
+    // Resto do código...
+};
+
+
+// Adicione um evento de clique à imagem "perdeu"
+perdeu.addEventListener('click', restartGame)
 
 
 const loop = setInterval(() => {
@@ -35,25 +60,39 @@ const loop = setInterval(() => {
 
     console.log(joaoPosition)
 
-    if (pipePosition <= 89 && pipePosition > 0 && joaoPosition < 105) {
+    if (pipePosition <= 89 && pipePosition > 0 && joaoPosition < 105) {  //só é executado quando o joao bate no pipe(perde o jogo)
 
-        pipe.style.animation = 'none'
+        pipe.style.animation = 'none' //o pipe perde a animação e fica parado
         pipe.style.left = `${pipePosition}px`
 
-        joao.style.animation = 'none'
+        joao.style.animation = 'none' //o joao fica sem animação e muda a imagem 
         joao.style.bottom = `${joaoPosition}px`
         joao.src = "./imagens/parado.jpg"
         
         joao.style.width = '75px'
         joao.style.marginLeft = '20px'
         
-        nuvens.style.animation = 'none'
+        nuvens.style.animation = 'none' //retira a animação das nuvens quando perde
         nuvens.style.left = `${nuvensPosition}px`
 
-        playPerdeuSound()
+        perdeu.src = "./imagens/perdeuotr.png" //adiciona uma imagem ao perder
+        perdeu.style.cursor = 'pointer'  //o cursor fica pointer
+
         
+
+        playPerdeuSound()
+        clearInterval(loop)
+
+
     }
 }, 10)
 
 
 document.addEventListener('keydown', jump)
+
+
+
+
+
+
+
